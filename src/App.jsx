f20b2afb54442b5dea8e601458dd38709c670771
aastikdas <aastikdas126@gmail.com>
2025-07-15 22:55:39 +0530

    interaction

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,38 @@ import Markdown from 'react-markdown'
 
 
 function App() {
-  
+
 
   const [code, setCode] = useState(`function sum()
 {
   return a+b;
 }`)
- const [review, setReview] = useState('')
+  const [review, setReview] = useState('')
+  const [error, setError] = useState(true)
+  const [reviewClick, setReviewClick] = useState(false)
 
   async function reviewCode() {
+    setReviewClick(true);        // Indicates button was clicked
+    setReview("");               // Clear previous review
+    setError(false);             // Reset error state
+
     try {
-      const res = await axios.post('https://ai-code-review-backend-45sx.onrender.com/ai/get-review',{code})
-      console.log(res.data);
-      setReview(res.data)
+      const res = await axios.post('https://ai-code-review-backend-45sx.onrender.com/ai/get-review', { code });
+      setReview(res.data);
     } catch (err) {
+      setError(true);
       setReview("Gemini is currently overloaded. Please wait a few seconds and click 'Review' again.");
       console.log(err);
-      console.log('hi');
     }
   }
+
 
   return (
     <>
       <main className='flex p-2 gap-4 bg-slate-600'>
         <div className="left w-[50vw] bg-amber-50 rounded-2xl p-1 basis-1/2 relative">
-        <span className='m-auto p-3 flex justify-center border-b-4 rounded-b-2xl text-xl font font-semibold'>Enter your Code..</span>
-          <div className="code h-[85vh] mt-2 overflow-y-scroll">
+          <span className='m-auto p-3 flex justify-center border-b-4 rounded-b-2xl text-xl font font-semibold'>Enter your Code..</span>
+          <div className="code h-[84vh] mt-2 overflow-y-scroll">
             <Editor
               value={code}
               onValueChange={code => setCode(code)}
@@ -48,19 +54,26 @@ function App() {
             />
           </div>
           <div className="review-btn">
-            <button 
-            className='bg-blue-400 p-3 rounded-2xl absolute bottom-3 right-8 hover:cursor-pointer'
-            onClick={reviewCode}
+            <button
+              className='bg-blue-400 p-3 rounded-2xl absolute bottom-3 right-8 hover:cursor-pointer'
+              onClick={reviewCode}
             >Review</button>
           </div>
         </div>
         <div className="right basis-1/2 w-[50vw] bg-amber-50   rounded-2xl p-2 ">
-        <span className='text-gray-700 flex justify-center border-b-4 rounded-b-2xl text-xl font font-semibold p-3'>AI Generated Review</span>
-          <div className="aihelp h-[85vh] mt-2 overflow-scroll px-4 ">
-              {
-              <Markdown>{review}</Markdown>
-              }
+          <span className='text-gray-700 flex justify-center border-b-4 rounded-b-2xl text-xl font font-semibold p-3'>AI Generated Review</span>
+          <div className="aihelp h-[84vh] my-2 overflow-scroll px-4 py-2 ">
+            {
+              review ? (
+                <Markdown>{review}</Markdown>
+              ) : reviewClick ? (
+                <div className='text-6xl text-gray-400 font-bold flex items-center justify-center h-[70vh]'>Review In Progress..</div>
+              ) : (
+                <div className='text-6xl text-gray-400 font-bold flex items-center justify-center h-[70vh]'>Review Appears Here</div>
+              )
+            }
           </div>
+
         </div>
       </main>
     </>
